8d9055ee1c83320c2854b4119ceaa2816a0ce659
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-01-03 20:36:19 +0530

    daa

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
   })
 );
 app.use(fileUpload( {useTempFiles: true,
-    tempFileDir: path.join(__dirname, "/tmp/"),}));
+    tempFileDir: path.join("/tmp"),}));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
