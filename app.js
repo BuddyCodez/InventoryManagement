82a9ed4963a0becf78002c345338e531b6560465
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-01-03 21:19:30 +0530

    dasdsada

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "tmp")));
+app.use("tmp", express.static("/tmp"));
 
 app.use("/", indexRouter);
 app.use("/logout", usersRouter);
